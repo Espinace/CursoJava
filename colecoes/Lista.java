caa3da9bf9ff0c16b8561538c5fa0c40f4066221
Bruno Amazonas Espinace <41213376+Espinace@users.noreply.github.com>
2020-10-08 16:28:52 -0300

    Add files via upload

---
+++ colecoes/Lista.java
@@ -0,0 +1,25 @@
+package colecoes;
+
+import java.util.ArrayList;
+
+public class Lista {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Usuario> lista = new ArrayList<Usuario>();
+		
+	    Usuario u1 = new Usuario("Ana");
+	    
+		lista.add(u1);
+		lista.add(new Usuario("Carlos"));
+		lista.add(new Usuario("Lia"));
+		lista.add(new Usuario("Bia"));
+		lista.add(new Usuario("Manu"));
+		
+		System.out.println(lista.get(3)); //acessar pelo indice
+		
+		for(Usuario u: lista) {
+			System.out.println(u.nome);
+		}
+	}
+}
---
+++ colecoes/Pilha.java
@@ -0,0 +1,25 @@
+package colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+
+	public static void main(String[] args) {
+		
+		Deque<String> livros = new ArrayDeque<String>();
+		
+		livros.add("O Pequeno Principe");
+		livros.push("Don QUixote");
+		livros.push("Rangers");
+		
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+		
+		System.out.println(livros.pop());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+	}
+}
